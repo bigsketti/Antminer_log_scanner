5cc7799dc8e09725b940bd19ea7f9fdcdeebb6f5
bigsketti <megerton04@gmail.com>
2024-05-07 14:52:12 -0400

    First functional version, more error checks to be added soon.

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,17 @@
 use std::process::Command;
 use std::io::{self, Write};
+use std::fs::File;
 use std::fs;
 
 fn main() {
     // SSH connection details
     let username = "miner";
     let mut hostname = String::new();
-    let command = "tail -n 1000 /var/log/messages.0"; // Command to execute on the SSH server
+//    let command = "tail -n 1000 /var/log/messages.0"; // Command to execute on the SSH server
+    let command = "cat /var/log/miner.log"; // Command to execute on the SSH server
 
     //read the host name
-    println!("Enter the miner's IP address: ");
+    println!("Enter the miner's IP address or q to quit: ");
     io::stdout().flush().unwrap();
     io::stdin().read_line(&mut hostname).expect("Failed to read line");
 
@@ -17,6 +19,7 @@ fn main() {
 
     if hostname.is_empty() {
         println!("No hostname provided");
+    } else if hostname == "q" {
         return;
     }
 
@@ -30,26 +33,34 @@ fn main() {
     // Check if the command executed successfully
     if output.status.success() {
         let stdout = String::from_utf8(output.stdout).expect("Invalid UTF-8 in stdout");
-        fs::write("\\miner_scan\\miner_log.txt", stdout).expect("Unable to write file");
+        println!("Logs:\n{}", stdout);
+        fs::write("miner_log.txt", stdout).expect("Unable to write file");
         println!("logs fetched and written, searching for errors\n")
     } else {
         let stderr = String::from_utf8(output.stderr).expect("Invalid UTF-8 in stderr");
         println!("Error:\n{}", stderr);
     }
 
-    const FAIL_SUBSTRINGS: Vec<String> = vec![]; // to be filled later with antminer error log messages
+    let fail_substrings: Vec<String> = vec![
+                                            String::from("chain avg vol drop"),
+                                            String::from("!!! reg crc error"),
+                                            String::from("fail to read pic")
+                                            ]; // to be filled later with antminer error log messages
 
-    let log = fs::read_to_string("\\miner_scan\\miner_log.txt").expect("Unable to read file");
+    let log = fs::read_to_string("miner_log.txt").expect("Unable to read file");
 
-    for i in FAIL_SUBSTRINGS {
-        if log.contains(&i) {
-            println!("{}", i);
-            return;
+    if log.is_empty() {
+        println!("no logs found\n")
+    } else {
+        println!("Errors found in logs:\n");
+        for i in fail_substrings {
+            if log.contains(&i) {
+                println!("{}", i);
+                return;
+            }
         }
     }
-
-    use std::fs::File;
     
-    File::create("/path/to/file.txt").expect("Unable to open file");
+    File::create("miner_log.txt").expect("Unable to open file");
 
 }
